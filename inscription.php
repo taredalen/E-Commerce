cfd926413d92bedb9d18316f0f772f63c646477e
clemence <clemence.rodrigues@esme.fr>
2021-05-21 09:28:09 +0200

    commit_21_05

--- inscription.php
+++ inscription.php
@@ -24,7 +24,7 @@ if(isset($_REQUEST['btn_register'])) //button name "btn_register"
         $errorMsg[]="Veuillez entrer votre nom";
     }
     if(empty($prenom)){
-        $errorMsg[]="Veuillez entrer votre prenom";
+        $errorMsg[]="Veuillez entrer votre prénom";
     }
     if(empty($rue)){
         $errorMsg[]="Veuillez entrer votre rue";
@@ -33,22 +33,22 @@ if(isset($_REQUEST['btn_register'])) //button name "btn_register"
         $errorMsg[]="Veuillez entrer votre code postal";
     }
     else if(!filter_var($mail, FILTER_VALIDATE_EMAIL)){
-        $errorMsg[]="Email format est incorrect!";
+        $errorMsg[]="le format de l'Email est incorrect!";
     }
     else if(!preg_match("/^0[0-9]{7}$/", $numero) ) {
-        $errorMsg[]="Format de numéro de téléphone est incorrect!";
+        $errorMsg[]="Le format du numéro de téléphone est incorrect!";
     }
     else if(!preg_match("/^[0-9]{5}$/", $code) ) {
-        $errorMsg[]="Format de code postal est incorrect!";
+        $errorMsg[]="Le format du code postal est incorrect!";
     }
     else if(strlen($password) < 8){
-        $errorMsg[] = "Mot de passe doit contenir au moins 8 caractères";
+        $errorMsg[] = "Le mot de passe doit contenir au moins 8 caractères";
     }
     else if(!preg_match("/^[A-Z][a-zA-Z0-9]*[a-z]$/", $password)){
-        $errorMsg[] = "Format de mot de passe est incorrect!";
+        $errorMsg[] = "Le format du mot de passe est incorrect!";
     }
     else if($password2 != $password){
-        $errorMsg[] = "Confirmation du mot de passe a échoué. Verifiez que vous avez entré le même mot de passe!";
+        $errorMsg[] = "La confirmation du mot de passe a échoué. Verifiez que vous avez entré le même mot de passe!";
     }
 
     else {
@@ -59,7 +59,7 @@ if(isset($_REQUEST['btn_register'])) //button name "btn_register"
             $row=$select_stmt->fetch(PDO::FETCH_ASSOC);
 
             if($row["mail"]==$mail){
-                $errorMsg[]="Le compre relié avec cet adresse mail existe déjà";
+                $errorMsg[]="Le compte relié avec cet adresse mail existe déjà";
             }
             else if(!isset($errorMsg)) {
                 $date_format=$date->format('Y-m-d');
@@ -80,7 +80,7 @@ if(isset($_REQUEST['btn_register'])) //button name "btn_register"
                     ':sexe' 	=>$sexe,
                     ':password' =>$new_password))){
 
-                    $registerMsg="Le profil enregistré avec succès. Ne quittez pas, vous serez redirigé vers la page d'authentification.";
+                    $registerMsg="Le profil a été enregistré avec succès. Ne quittez pas, vous serez redirigé vers la page d'authentification.";
                     header("refresh:4;authentification.php");	//refresh 2 second after redirect to "authentification.php" page
                 }
             }
@@ -173,21 +173,21 @@ if(isset($_REQUEST['btn_register'])) //button name "btn_register"
                 <form method="post" action="inscription.php">
                     <div class="row">
                         <div class="form-group col-md-6">
-                            <label for="nom" class="text-info"  style="color: #19b9cc">Nom</label>
+                            <label for="nom" class="text-info"  style="color: #19b9cc">Nom*</label>
                             <input type="text" class="form-control" id="nom" name="nom" minlength="2" placeholder="Hewlett">
                         </div>
                         <div class="form-group col-md-6">
-                            <label for="prenom" class="text-info" style="color: #19b9cc">Prenom</label>
+                            <label for="prenom" class="text-info" style="color: #19b9cc">Prenom*</label>
                             <input type="text" class="form-control" id="prenom" name="prenom" minlength="2" placeholder="William">
                         </div>
                     </div>
                     <div class="row">
                         <div class="form-group col-md-6">
-                            <label for="mail" class="text-info" style="color: #19b9cc">Email</label>
+                            <label for="mail" class="text-info" style="color: #19b9cc">Email*</label>
                             <input type="email" class="form-control" id="mail" name="mail" placeholder="hewlett.william@gmail.com">
                         </div>
                         <div class="form-group col-md-6">
-                            <label for="numero" class="text-info" style="color: #19b9cc">Numéro de téléphone</label>
+                            <label for="numero" class="text-info" style="color: #19b9cc">Numéro de téléphone*</label>
                             <input type="tel" class="form-control" id="numero" name="numero" minlength="8" maxlength="8">
                             <small id="numberHelpBlock" class="form-text text-muted">
                                 Doit contenir 8 chiffres et commencer par 0</small>
@@ -195,15 +195,15 @@ if(isset($_REQUEST['btn_register'])) //button name "btn_register"
                     </div>
                     <div class="row">
                         <div class="form-group col-md-6">
-                            <label for="rue" class="text-info" style="color: #19b9cc">Rue</label>
+                            <label for="rue" class="text-info" style="color: #19b9cc">Rue*</label>
                             <input type="text" class="form-control" id="rue" name="rue" placeholder=" 14 rue de la Verrerie">
                         </div>
                         <div class="form-group col-md-4">
-                            <label for="ville" class="text-info" style="color: #19b9cc">Ville</label>
+                            <label for="ville" class="text-info" style="color: #19b9cc">Ville*</label>
                             <input type="text" class="form-control" id="ville" name="ville" placeholder="Meudon">
                         </div>
                         <div class="form-group col-md-2">
-                            <label for="code" class="text-info" style="color: #19b9cc">Code Postal</label>
+                            <label for="code" class="text-info" style="color: #19b9cc">Code Postal*</label>
                             <input type="text" class="form-control" id="code" name="code" minlength="5" maxlength="5" placeholder="92197">
                         </div>
                     </div>
@@ -235,14 +235,14 @@ if(isset($_REQUEST['btn_register'])) //button name "btn_register"
                     </div>
                     <div class="row">
                         <div class="form-group col-md-6">
-                            <label for="password" class="text-info" style="color: #19b9cc">Mot de Passe</label>
+                            <label for="password" class="text-info" style="color: #19b9cc">Mot de Passe*</label>
                             <input type="password" class="form-control" id="password" name="password" minlength="8" aria-describedby="passwordHelpBlock">
                             <small id="passwordHelpBlock" class="form-text text-muted">
                                 Doit contenir au moins 8 caractères, commencer par une lettre majuscule et se terminer par un lettre minuscule, sans caractères spéciaux.
                             </small>
                         </div>
                         <div class="form-group col-md-6">
-                            <label for="password2" class="text-info" style="color: #19b9cc">Confirmation mot de passe</label>
+                            <label for="password2" class="text-info" style="color: #19b9cc">Confirmation mot de passe*</label>
                             <input type="password" class="form-control" id="password2" name="password2">
                         </div>
                     </div>
