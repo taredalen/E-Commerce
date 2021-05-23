580f78742a3a840a0a8be54b57a3388eb60ab8bf
sabrineTT <sabrine.thibert@esme.fr>
2021-05-23 15:55:28 +0200

    2021_05_23 repense de gestion_commande & ajout de panier_client

--- ajout_panier.php
+++ ajout_panier.php
@@ -6,16 +6,30 @@ session_start();
 $connect = mysqli_connect("localhost", "root", "", "ProjectPHP");
 $id = $_GET['id']; // get id through query string
 
+//Création de la table panier du client, qui se supprimera lorsque celui l'aura validé
+$id_client = $_SESSION['user_login'];
+$panier = "CREATE TABLE if not exists Panier_".$id_client."(
+                         id_client INT UNSIGNED,
+                         id_produit INT UNSIGNED,
+                         quantite_produit INT,
+                         date_commande DATE,
+                         prix FLOAT,
+                         FOREIGN KEY(id_client) REFERENCES Client(id),
+                         FOREIGN KEY(id_produit) REFERENCES Produits(id))";
+$res_panier = mysqli_query($connect, $panier); //Création des commandes
+
+//Début de l'ajout au panier lorsque le client clique sur "commande"
 $sql1 = "SELECT stock FROM Produits WHERE id='".$id."'";
 $stmt0 = mysqli_prepare($connect,$sql1);
 mysqli_stmt_execute($stmt0);
 $result = mysqli_stmt_get_result($stmt0);
 
 while ($row = mysqli_fetch_assoc($result)){
+
     $stock = $row['stock'];
     if($_POST['quantite']>$stock){
         mysqli_close($connect); // Close connection
-        $errorMsg="Veuillez entrer une quantité du produit valide";
+        $errorMsg="Veuillez entrer une quantité du produit inférieur au stock disponible";
         header("location:gestion_commande.php?errorMsg=$errorMsg"); // redirects to liste_produit page
         exit;
     }
@@ -38,7 +52,7 @@ while ($row = mysqli_fetch_assoc($result)){
         $qtn = $_POST['quantite'];
         $id_client = $_SESSION['user_login'];
         $prix_produit = "SELECT prix*'{$qtn}' FROM Produits WHERE id='{$id}'";
-        $sql = "INSERT INTO Commande (id_client, id_produit, quantite_produit, date_commande, prix) VALUES ($id_client, $id, $qtn, CURRENT_DATE(), ($prix_produit))";
+        $sql = "INSERT INTO Panier_".$id_client." (id_client, id_produit, quantite_produit, date_commande, prix) VALUES ($id_client, $id, $qtn, CURRENT_DATE(), ($prix_produit))";
         $stmt = mysqli_prepare($connect, $sql);
         if ($stmt->execute()) {
             $sql1 = "UPDATE Produits SET stock = stock-'{$qtn}' WHERE id='{$id}'";
@@ -50,7 +64,7 @@ while ($row = mysqli_fetch_assoc($result)){
             exit;
         } else {
             mysqli_close($connect); // Close connection
-            $errorMsg[] = "Erreur";
+            $errorMsg = mysqli_stmt_error($stmt);
             header("location:gestion_commande.php?errorMsg=$errorMsg"); // redirects to liste_produit page
             exit;
         }
