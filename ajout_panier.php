884f37ba0344935a9263a613f82b79dac09fff28
sabrineTT <sabrine.thibert@esme.fr>
2021-05-23 02:12:31 +0200

    2021_05_22 gestion commande marche :)

--- ajout_panier.php
+++ ajout_panier.php
@@ -7,22 +7,43 @@ $connect = mysqli_connect("localhost", "root", "", "ProjectPHP");
 $id = $_GET['id']; // get id through query string
 
 $sql1 = "SELECT stock FROM Produits WHERE id='".$id."'";
-$result = mysqli_query($connect, $sql1);
-if (mysqli_num_rows($result) > 0){
-    $quantite = $_POST['quantite'];
-    if(empty($quantite) || $quantite=0){
+$stmt0 = mysqli_prepare($connect,$sql1);
+mysqli_stmt_execute($stmt0);
+$result = mysqli_stmt_get_result($stmt0);
+
+while ($row = mysqli_fetch_assoc($result)){
+    $stock = $row['stock'];
+    if($_POST['quantite']>$stock){
         mysqli_close($connect); // Close connection
-        $errorMsg[]="Veuillez entrer une quantité du produit";
+        $errorMsg="Veuillez entrer une quantité du produit valide";
         header("location:gestion_commande.php?errorMsg=$errorMsg"); // redirects to liste_produit page
         exit;
     }
-    else {
+
+    elseif($_POST['quantite']==0){
+        mysqli_close($connect); // Close connection
+        $errorMsg="Veuillez entrer une quantité du produit valide";
+        header("location:gestion_commande.php?errorMsg=$errorMsg"); // redirects to liste_produit page
+        exit;
+    }
+
+    elseif(empty($_POST['quantite'])){
+        mysqli_close($connect); // Close connection
+        $errorMsg="Veuillez entrer une quantité du produit valide";
+        header("location:gestion_commande.php?errorMsg=$errorMsg"); // redirects to liste_produit page
+        exit;
+    }
+
+    else{
         $qtn = $_POST['quantite'];
         $id_client = $_SESSION['user_login'];
         $prix_produit = "SELECT prix*'{$qtn}' FROM Produits WHERE id='{$id}'";
         $sql = "INSERT INTO Commande (id_client, id_produit, quantite_produit, date_commande, prix) VALUES ($id_client, $id, $qtn, CURRENT_DATE(), ($prix_produit))";
         $stmt = mysqli_prepare($connect, $sql);
         if ($stmt->execute()) {
+            $sql1 = "UPDATE Produits SET stock = stock-'{$qtn}' WHERE id='{$id}'";
+            $stmt1 = mysqli_prepare($connect, $sql1);
+            $stmt1->execute();
             mysqli_close($connect); // Close connection
             $successMsg = "Produit ajouté au panier avec succès";
             header("location:gestion_commande.php?successMsg=$successMsg"); // redirects to liste_produit page
